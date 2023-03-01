e5f649ff3c68025278bd2a089f4f0d13d9edc12d
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-28 23:24:14 -0600

    modified Author model

--- population.js
+++ population.js
@@ -12,6 +12,9 @@ const Author = mongoose.model('Author', new mongoose.Schema({
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
+  author: { 
+    type: mongoose.Schema.Types.ObjectId, ref: 'Author' 
+  }
 }));
 
 async function createAuthor(name, bio, website) { 
@@ -42,8 +45,8 @@ async function listCourses() {
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor('Mosh', 'My bio', 'My Website');
 
-// createCourse('Node Course', 'authorId')
+createCourse('Node Course', '63fee09e98f1c9d6d1e3cafb')
 
 // listCourses();
